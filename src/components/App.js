3c005ae179858990b5b794ab0b41e3663084a893
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-22 13:14:21 -0600

    Refactor app into stateless functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-const mainContainer = document.querySelector('#main');
-
-class App extends React.Component {
-  render(){
-    return(
-      <div id = "calculator-holder">
-        <Display />
-        <ButtonPanel />
-      </div>
-    )
-  }
+export const App = (props) => {
+  return(
+    <div id = "calculator-holder">
+      <Display />
+      <ButtonPanel />
+    </div>
+  )
 }
 
-ReactDOM.render(<App/>, mainContainer);
