34cdd4c86a2074668f497a363184faab692ecee6
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-22 13:04:44 -0600

    Write buttons panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,2 +1,34 @@
 import React from 'react';
 import Button from './Button';
+
+export class ButtonPanel extends React.Component {
+  renderButtons() {
+    const groups = [
+      ['AC', '+/-', '%', '+'],
+      ['7', '8', '9', 'X'],
+      ['4', '5', '6', '-'],
+      ['1', '2', '3', '+'],
+      ['0', '.', '=', null]
+    ]
+    for (let i = 0; i < groups.length; i++){
+      const [a,b,c,d] = groups[i];
+
+      return(
+        <div>
+          <Button value = {a} className = 'button'/>
+          <Button value = {b} className = 'button'/>
+          <Button value = {c} className = {d ? 'button' : 'button orange-btn'} />
+          {d && <Button value = {d} className = 'button orange-btn'/>}
+        </div>
+      )
+    }    
+  }
+  
+  render(){
+    return(
+      <div class = "button-panel row">
+        {this.renderButtons}
+      </div>
+    )
+  }
+}
