4710d100d23993c28444dd16b966d968b8c10493
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-24 11:51:58 -0600

    Write display styles

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// It should have a gray background.
+// It should have a height set to 100px.
+// The result should be presented with a white and bold text.
+// The result should have padding from the edges.
+// The result should be aligned to the right.
+
+const displayStyles = {
+  backgroundColor: '#AAA',
+  height: '100px',
+  width: '100%',
+  color: '#FFF',
+  fontWeight: 'bold',
+  padding: '1em .5em',
+  textAlign: 'right'
+}
+
 export const Display = (props) =>
-  <div className="calc-display">
+  <div className="calc-display" style={displayStyles}>
     <p>{props.result}</p>
   </div>
 
