03b33de6ad09536299198813671c746b1b76179e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-24 11:51:23 -0600

    Add styles for calc width

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import {ButtonPanel} from './ButtonPanel';
 import {Display} from './Display';
 
+const calcStyles = {
+  width: '700px',
+}
+
 export const App = (props) => {
   return(
-    <div id = "calculator-holder">
+    <div id="calculator-holder" style={calcStyles}>
       <Display />
       <ButtonPanel />
     </div>
