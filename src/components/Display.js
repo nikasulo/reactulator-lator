349d8e351095083531dc2481fde7eb765b5fe262
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-24 10:19:37 -0600

    fix lint errors

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Display = (props) => { 
-  return(
-    <div className = 'calc-display'>
-      <p>{props.result}</p>
-    </div>
-  )
-}
+export const Display = (props) =>
+  <div className="calc-display">
+    <p>{props.result}</p>
+  </div>
 
 Display.propTypes = {
   result: PropTypes.string
